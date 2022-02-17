5fbb09af673da5d4974c20e53f3ba42ce64a6361
Austin <madeinsg3@gmail.com>
2022-02-17 17:02:42 +1100

    CHANGE api address

--- src/global.js
+++ src/global.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-const apiAddress = "http://192.168.20.32:5000";
+// const apiAddress = "http://192.168.20.32:5000";
+const apiAddress = "http://randomnomad.xyz:5000";
 
 const setCookie = (name, value, days) => {
   var expires = "";
